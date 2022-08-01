1c6b66e114c61a6957ec6d688a7245e2d9d56d33
akashtyagi <akashdktyagi@gmail.com>
2022-08-01 19:22:54 +0800

    Added getAll and get by email end point and repository method

--- src/main/java/com/automationfraternity/patientapi/repository/PatientRepository.java
+++ src/main/java/com/automationfraternity/patientapi/repository/PatientRepository.java
@@ -3,6 +3,8 @@ package com.automationfraternity.patientapi.repository;
 import com.automationfraternity.patientapi.model.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * JpaRepository is a JPA (Java Persistence API) specific extension of Repository.
  * It contains the full API of CrudRepository and PagingAndSortingRepository.
@@ -10,4 +12,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * ID: Type of the id of the entity that repository manages (Generally the wrapper class of your @Id that is created inside the Entity/Model class)
  */
 public interface PatientRepository extends JpaRepository<Patient,Long> {
+    List<Patient> findByEmail(String email);
 }
